5c0cd58934cb90fd08608ae919bfa71f6aed5473
Cedric Meury <cedric@meury.com>
2013-08-30 13:30:58 +0200

    list manipulation

--- foo/src/ec_lists.erl
+++ foo/src/ec_lists.erl
@@ -4,6 +4,7 @@
 
 -module(ec_lists).
 -export([print_each/1]).
+-export([twomult/1]).
 
 print_each(L) ->
 	print_each(L, 1).
@@ -11,6 +12,8 @@ print_each(L) ->
 print_each([A|B], Nr) ->
 	io:format("~p is ~p~n", [Nr, A]),
 	print_each(B, Nr+1);
-print_each(X, Nr) when length(X) == 0 ->
+print_each(X, _Nr) when length(X) == 0 ->
 	ok.
 
+twomult(L) ->
+  lists:map(fun(A) -> A*2 end, L).
