45b486cd175bdbc4ca775857436e8274c2251841
Cedric Meury <cedric@meury.com>
2013-08-30 11:28:05 +0200

    tail recursion function

--- foo/src/ec_recur.erl
+++ foo/src/ec_recur.erl
@@ -3,12 +3,18 @@
 %%% @copyright 2013 Cedric-Meury.ch
 
 -module(ec_recur).
+-export([sum_seq/2]).
 -export([sum_seq/1]).
 
 %% @doc recursive sum
--spec(sum_seq(number()) -> number()).
-sum_seq(L) when L == 1 ->
+-spec(sum_seq(number(),number()) -> number()).
+sum_seq(0, L) ->
 	L;
-sum_seq(L) ->
-	L + sum_seq(L-1).
+sum_seq(N, L) ->
+	sum_seq(N - 1, L + N).
+
+%% @doc convenience function
+-spec(sum_seq(number()) -> number()).
+sum_seq(N) ->
+	sum_seq(N, 0).
 
